5adb2cf93d4a4154ae6e6db92abc8c17eef052fe
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-07 23:44:30 +0700

    added SoftAssertionsTest.java

---
+++ src/test/java/SoftAssertionsTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+public class SoftAssertionsTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1600x900";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void softAssertionsTest() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $$("ul").findBy(text("Soft assertions")).shouldBe(visible);
+        $$("ul a").findBy(text("Soft assertions")).click();
+        $(".markdown-body").$(withText("JUnit5")).shouldBe(visible);
+
+
+    }
+}
