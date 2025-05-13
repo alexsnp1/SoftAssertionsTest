13985cd77049f3a51bdba21f382461853987b21b
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-14 00:05:16 +0700

    updated SoftAssertionsTest.java

--- src/test/java/SoftAssertionsTest.java
+++ src/test/java/SoftAssertionsTest.java
@@ -2,10 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
@@ -30,22 +28,34 @@ public class SoftAssertionsTest {
         //$(".markdown-body").$(withText("JUnit5")).shouldBe(visible);
         //проверить что внутри есть пример кода для JUnit5
         String expectedCode = """
-                Using JUnit5 extend test class:
-                @ExtendWith({SoftAssertsExtension.class})
-                class Tests {
-                  @Test
-                  void test() {
-                    Configuration.assertionMode = SOFT;
-                    open("page.html");
+                 Using JUnit5 extend test class:
+                 @ExtendWith({SoftAssertsExtension.class})
+                 class Tests {
+                   @Test
+                   void test() {
+                     Configuration.assertionMode = SOFT;
+                     open("page.html");
                 
-                    $("#first").should(visible).click();
-                    $("#second").should(visible).click();
-                  }
-                }
-               """;
+                     $("#first").should(visible).click();
+                     $("#second").should(visible).click();
+                   }
+                 }
+                """;
         $("div.markdown-body").shouldHave(text(expectedCode));
+    }
 
+    //тест на закрытие попапа
+        @Test
+        void closeCookiePopup() {
+        open("https://www.otpbank.ru/");
+        //проверка что это окно изначально есть
+        $(withText("Продолжая просмотр сайта, вы даете согласие на обработку")).shouldBe(visible);
+        // по тегу и тексту для уверенности
+        $(byTagAndText("strong", "Закрыть" )).click();
+        //проверка что оно закрылось
+        $(withText("Продолжая просмотр сайта, вы даете согласие на обработку")).shouldBe(hidden);
+
+        }
 
 
     }
-}
